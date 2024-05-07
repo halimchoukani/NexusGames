ab51f24a7c8d0832baf21d22891b06a17a087d64
Halim Choukani <halim.choukani@gmail.com>
2024-05-07 11:06:00 +0100

    fix les bug du css et ajouter l'algorithme de panier

---
+++ src/Controller/CartController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CartController extends AbstractController
+{
+    #[Route('/cart', name: 'app_cart')]
+    public function index(Request $request): Response
+    {
+        $session = $request->getSession();
+        $panier = $session->get('panier', []);
+        $panierData = [];
+        foreach ($panier as $id => $quantity) {
+            $panierData[] = [
+                'id' => $id,
+                'quantity' => $quantity
+            ];
+        }
+        return $this->render('cart/index.html.twig', [
+            $panierData
+        ]);
+    }
+    #[Route('/add', name: 'add_cart')]
+    public function add($id, Request $request)
+    {
+        $session = $request->getSession();
+        $panier = $session->get('panier', []);
+        if (!empty($panier[$id]))
+            $panier[$id]++;
+        else
+            $panier[$id] = 1;
+        $session->set('panier', $panier);
+        return $this->redirectToRoute('cart');
+    }
+}

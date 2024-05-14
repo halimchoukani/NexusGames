f16286321c0b4b06c131a1eadb8d85ae765a82c2
Halim Choukani <halim.choukani@gmail.com>
2024-05-14 11:02:37 +0100

    ajouter add product et afficher tous les produits

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProductRepository;
+use App\Entity\Product;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\ProduitType;
+use Doctrine\Persistence\ManagerRegistry;
+
+class ProductController extends AbstractController
+{
+    private $doctrine;
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+
+    #[Route('/products', name: 'app_produit')]
+    public function index(ProductRepository $productRepository): Response
+    {
+        $produits = $productRepository->findAll();
+
+        return $this->render('product/index.html.twig', [
+            'products' => $produits,
+            'image_directory' => $this->getParameter('images_directory'),
+        ]);
+    }
+
+    #[Route('/product/{id}', name: 'app_produit_show')]
+    public function show($id, ProductRepository $productRepository): Response
+    {
+        $product = $productRepository->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException('Product not found');
+        }
+
+        return $this->render('product/produit.html.twig', [
+            'product' => $product,
+            'image_directory' => $this->getParameter('images_directory'),
+        ]);
+    }
+
+    #[Route('/product/add', name: 'app_produit_ajouter')]
+    public function ajouter(Request $request): Response
+    {
+        $produit = new Product();
+        $form = $this->createForm(ProduitType::class, $produit);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $produit = $form->getData();
+            //****************Manage Uploaded FileName
+            $photo_prod = $form->get('image')->getData();
+            $originalFilename = $photo_prod->getClientOriginalName();
+            $newFilename = $originalFilename . '-' . uniqid() . '.' . $photo_prod->getClientOriginalExtension();
+            $photo_prod->move($this->getParameter('images_directory'), $newFilename);
+            $produit->setImage($newFilename);
+            //****************
+            $entityManager = $this->doctrine->getManager();
+            $entityManager->persist($produit);
+            $entityManager->flush();
+        }
+        return $this->render('product/ajouter.html.twig', ['form' => $form->createView(),]);
+    }
+}
---
+++ src/Form/ProduitType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\NotNull;
+
+class ProduitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('marque')
+            ->add('description')
+            ->add('prix', MoneyType::class)
+            ->add('quantite', null, [
+                'attr' => [
+                    'min' => 0,
+                ],
+            ])
+            ->add(
+                'image',
+                FileType::class,
+                [
+                    'constraints' => new File([
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/jpg',
+                            'image/png',
+                        ]
+                    ])
+                ]
+            )
+            ->add('date_insertion', DateType::class)
+            ->add('ajouter', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
